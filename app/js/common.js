09d0a22b1c584bea771a321a31d9b58dc23b785f
Korobeynik <korobeynik77@gmail.com>
2019-03-25 11:44:23 +0200

    25 Update

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
 
 function screenClass() {
+
     if($(window).innerWidth() > 960) {
         $('.services-grid').removeClass('owl-carousel');
     } else {
@@ -16,11 +17,78 @@ function screenClass() {
                 autoplayHoverPause: false
             });
     }
+
+    if($(window).innerWidth() > 720) {
+        $('.news-slider').owlCarousel('destroy'); 
+    } else {
+        $('.news-slider').owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 0,
+      smartSpeed: 500,
+      nav: false,
+      dots: true,
+      autoplayTimeout: 20000,
+      autoplayHoverPause: false
+      });
+    }
+
+  if ($(window).innerWidth() < 1200) {
+    $('.cooperation-slider').owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 0,
+      smartSpeed: 500,
+      nav: false,
+      dots: true,
+      autoplayTimeout: 20000,
+      autoplayHoverPause: false,
+      responsive : {
+        0 : {
+          items: 1,
+          loop: true,
+          margin: 0,
+          smartSpeed: 500,
+          nav: false,
+          dots: true,
+          autoplayTimeout: 20000,
+          autoplayHoverPause: false
+        },
+        720 : {
+          items: 1,
+          loop: true,
+          margin: 0,
+          smartSpeed: 500,
+          nav: false,
+          dots: true,
+          autoplayTimeout: 20000,
+          autoplayHoverPause: false
+        }
+    }
+    });
+    $('.cooperation-slider .tab-content').show();
+  } else {
+    $('.cooperation-slider').owlCarousel('destroy');
+    // Init tab
+    $('.tab-content').hide();
+    $('.tab-content:first').show();
+    $('.cooperation-tabs li:first').addClass('active');
+    $('.cooperation-tabs li').click(function (event) {
+      event.preventDefault();
+      $('.cooperation-tabs li').removeClass('active');
+      $(this).addClass('active');
+      $('.tab-content').hide();
+
+      var selectTab = $(this).find('a').attr("href");
+
+      $(selectTab).fadeIn();
+    });
+  }
+
+
 }
 
-
 screenClass();
-
 
 $(window).bind('resize',function(){
     screenClass();
@@ -106,5 +174,46 @@ $(document).ready(function(){
 		// 	}
 		// }
 	});
+
+  $(".manufactures-slider").owlCarousel({
+    items: 8,
+    loop: true,
+    margin: 15,
+      nav: true,
+      dots: true,
+      smartSpeed: 700,
+      navText: ['<svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38"> <g fill="none" fill-rule="evenodd" transform="translate(1 1)"> <circle cx="18" cy="18" r="18" stroke="#EFF1F1" stroke-width="2"/> <path fill="#000" d="M20.23 13.16a.537.537 0 0 0-.766 0 .541.541 0 0 0 0 .76l3.881 3.88H10.537a.541.541 0 0 0 0 1.081h12.808l-3.881 3.873a.55.55 0 0 0 0 .765.537.537 0 0 0 .765 0l4.8-4.8a.525.525 0 0 0 0-.756l-4.8-4.802z"/> </g> </svg>','<svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38"> <g fill="none" fill-rule="evenodd" transform="translate(1 1)"> <circle cx="18" cy="18" r="18" stroke="#EFF1F1" stroke-width="2"/> <path fill="#000" d="M20.23 13.16a.537.537 0 0 0-.766 0 .541.541 0 0 0 0 .76l3.881 3.88H10.537a.541.541 0 0 0 0 1.081h12.808l-3.881 3.873a.55.55 0 0 0 0 .765.537.537 0 0 0 .765 0l4.8-4.8a.525.525 0 0 0 0-.756l-4.8-4.802z"/> </g> </svg>'],
+      responsiveClass: true,
+      responsive : {
+        0 : {
+          items: 2,
+          loop: true,
+          margin: 0,
+          smartSpeed: 500,
+          nav: false,
+          dots: true,
+          autoplayTimeout: 20000,
+          autoplayHoverPause: false
+        },
+        480 : {
+          items: 3
+        },
+        600 : {
+          items: 4
+        },
+        720 : {
+          items: 4
+        },
+        900 : {
+          items: 6
+        },
+        1100 : {
+          items: 6
+        },
+        1200 : {
+          items: 8
+        }
+    }
+  });
 
 });
